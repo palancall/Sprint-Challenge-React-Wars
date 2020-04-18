c6425863de27929d0c6157f2376e5b662e8ff55f
Luis <nettalkangola@yahoo.com>
2020-04-17 22:14:26 -0400

    completed MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Character from "./components/Character";
+import axios from "axios";
+import "./index.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [characters, setCharacters] = useState([]);
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then((response) => {
+        console.log(response.data.results);
+        setCharacters(response.data.results);
+      })
+      .catch((error) => {
+        console.log("error!", error);
+      });
+  }, []);
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      {characters.map((character) => {
+        return <Character character={character} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,64 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+  width: 300px;
+  height: 550px;
+  background: azure;
+  margin: 30px;
+  border-radius: 10px;
+  -webkit-box-shadow: 5px 5px 15px 5px #000000;
+  box-shadow: 5px 5px 15px 5px #000000;
+`;
+
+const DescriptionDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1% 0;
+  font-size: 16px;
+`;
+const CardBody = styled.div`
+  padding: 5%;
+`;
+
+const Character = (props) => {
+  const character = props.character;
+  console.log(character);
+
+  const Image = styled.img`
+    border-radius: 5px 5px 0 0;
+  `;
+
+  const ImageDiv = styled.div``;
+
+  return (
+    <div>
+      <Card>
+        <ImageDiv>
+          <Image src={character.image} alt={character.name} />
+        </ImageDiv>
+        <CardBody>
+          <h2>{character.name}</h2>
+          <DescriptionDiv>
+            <span>Gender:</span>
+            <span>{character.gender}</span>
+          </DescriptionDiv>
+          <DescriptionDiv>
+            <span>Species:</span>
+            <span>{character.species}</span>
+          </DescriptionDiv>
+          <DescriptionDiv>
+            <span>Location:</span>
+            <span>{character.location.name}</span>
+          </DescriptionDiv>
+          <DescriptionDiv>
+            <span>Status:</span>
+            <span>{character.status}</span>
+          </DescriptionDiv>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Character;
